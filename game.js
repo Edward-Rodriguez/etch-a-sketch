c73f9329c5a44fb63db32bc9f4ceb50c5345fe5c
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-07-28 12:39:53 -0400

    Add background color change effect on pixel div hover

--- game.js
+++ game.js
@@ -1,1 +1,29 @@
+const containerDiv = document.querySelector('#container');
 
+function generateSquareGrid(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const pixelDiv = document.createElement('div');
+      pixelDiv.classList.add('border');
+      pixelDiv.style.width = `${calculateDimensions(n)}%`;
+      pixelDiv.style.height = `${calculateDimensions(n)}%`;
+      containerDiv.appendChild(pixelDiv);
+    }
+  }
+}
+
+function calculateDimensions(n) {
+  return (1 / n) * 100;
+}
+
+function setEventListeners() {
+  const pixelDivs = document.querySelectorAll('#container div');
+  pixelDivs.forEach((pixelDiv) => {
+    pixelDiv.addEventListener('mouseover', () => {
+      pixelDiv.classList.add('on-hover');
+    });
+  });
+}
+
+generateSquareGrid(128);
+setEventListeners();
