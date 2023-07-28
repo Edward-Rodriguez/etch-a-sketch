9b7f8ed371c3cdb4ec69d0dceb338c2c3ae4f420
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-07-28 15:50:43 -0400

    Add button and functionality to change grid size on user input

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 const containerDiv = document.querySelector('#container');
 
-function generateSquareGrid(n) {
+function generateSquareGrid(n = 16) {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       const pixelDiv = document.createElement('div');
-      pixelDiv.classList.add('border');
       pixelDiv.style.width = `${calculateDimensions(n)}%`;
       pixelDiv.style.height = `${calculateDimensions(n)}%`;
       containerDiv.appendChild(pixelDiv);
@@ -16,14 +15,50 @@ function calculateDimensions(n) {
   return (1 / n) * 100;
 }
 
-function setEventListeners() {
+function setPixelDivsEventListener() {
   const pixelDivs = document.querySelectorAll('#container div');
   pixelDivs.forEach((pixelDiv) => {
     pixelDiv.addEventListener('mouseover', () => {
-      pixelDiv.classList.add('on-hover');
+      pixelDiv.classList.add('hovered');
     });
   });
 }
 
-generateSquareGrid(128);
-setEventListeners();
+const changeGridSizeButton = document.querySelector('#grid-size-btn');
+changeGridSizeButton.addEventListener('click', () => {
+  gridSize = getUserInput();
+  if (gridSize > 0) {
+    containerDiv.replaceChildren();
+    generateSquareGrid(gridSize);
+    setPixelDivsEventListener();
+  }
+});
+
+function getUserInput() {
+  let userInput = prompt(
+    'Please choose a value for n between 1 and 100, for a grid size (n x n)'
+  );
+  if (isNull(userInput)) return -1;
+  else if (isValidInput(userInput)) return +userInput;
+  else {
+    while (!isValidInput(userInput)) {
+      userInput = prompt('Enter a valid value between 1 and 100');
+    }
+  }
+  return +userInput;
+}
+
+function isValidInput(input) {
+  if (isNull(input)) return true;
+  input = parseInt(input);
+  if (isNaN(input) || !(input >= 1 && input <= 100)) return false;
+  else return true;
+}
+
+function isNull(input) {
+  if (input === null || input === '') return true;
+  else return false;
+}
+
+generateSquareGrid();
+setPixelDivsEventListener();
